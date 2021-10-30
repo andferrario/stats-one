585fd32af64ace29bb7408a91c6e4b0575935728
aferrario <andrea.ferrario@lastminute.com>
2021-10-30 20:15:52 +0200

    first commit

---
+++ src/main/java/com/andfer/statsone/StatsOneApplication.java
@@ -0,0 +1,13 @@
+package com.andfer.statsone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatsOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StatsOneApplication.class, args);
+    }
+
+}
